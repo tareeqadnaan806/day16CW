a90d0c3061bd70171384068e96cc9f2b108c4ff1
Adnaan <112771255+tareeqadnaan806@users.noreply.github.com>
2023-09-13 01:16:41 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const fname = prompt("Enter your First name");
+const lname = prompt("Enter your Last name");
+const pno = prompt("Enter your phone number");
+const village = prompt("Enter your village");
+const city = prompt("Enter your city");
+const state = prompt("Enter your state");
+const country = prompt("Enter your country");
+
+const userData = {
+  fname,
+  lname,
+  pno,
+  village,
+  city,
+  state,
+  country,
+};
+
+// console.log(userData);
+localStorage.setItem("user data", JSON.stringify(userData));
+
+const data = JSON.parse(localStorage.getItem("user data"));
+// console.log(data);
+
+document.getElementById("fname").innerText = data.fname;
+document.getElementById("lname").innerText = data.lname;
+document.getElementById("phn").innerText = data.phn;
+document.getElementById("village").innerText = data.village;
+document.getElementById("city").innerText = data.city;
+document.getElementById("state").innerText = data.state;
+document.getElementById("country").innerText = data.country;
